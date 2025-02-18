4fb4c733292d8e8bd96c34c4370d71e576963e0b
spatiumstas <79056064+spatiumstas@users.noreply.github.com>
2025-02-18 23:21:38 +0300

    fix installer

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 REPO="keensnap"
 SCRIPT="keensnap.sh"
 SNAPD="keensnap-init"
+CONFIG="config.template"
 TMP_DIR="/tmp"
 OPT_DIR="/opt"
 KEENSNAP_DIR="/opt/root/KeenSnap"
@@ -22,6 +23,10 @@ ln -sf $KEENSNAP_DIR/$SCRIPT $OPT_DIR/bin/$REPO
 curl -L -s "https://raw.githubusercontent.com/spatiumstas/$REPO/main/$SNAPD" --output $TMP_DIR/$SNAPD
 mv "$TMP_DIR/$SNAPD" "$KEENSNAP_DIR/$SNAPD"
 chmod +x $KEENSNAP_DIR/$SNAPD
+
+curl -L -s "https://raw.githubusercontent.com/spatiumstas/$REPO/main/$CONFIG" --output $TMP_DIR/$CONFIG
+mv "$TMP_DIR/$CONFIG" "$KEENSNAP_DIR/config.sh"
+chmod +x $KEENSNAP_DIR/config.sh
 
 URL=$(echo "aHR0cHM6Ly9sb2cuc3BhdGl1bS5rZWVuZXRpYy5wcm8=" | base64 -d)
 JSON_DATA="{\"script_update\": \"KeenSnap_install\"}"
